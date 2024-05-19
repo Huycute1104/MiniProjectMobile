6df7c4f565bea1d74ae2adf113152db48beb056d
Huy Pham <huypt110402@gmail.com>
2024-05-19 16:09:03 +0700

    add logout

--- app/src/main/java/com/example/miniproject/RaceActivity.java
+++ app/src/main/java/com/example/miniproject/RaceActivity.java
@@ -1,11 +1,15 @@
 package com.example.miniproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RaceActivity extends AppCompatActivity {
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,18 @@ public class RaceActivity extends AppCompatActivity {
 
         TextView txtCoins = findViewById(R.id.txtCoins);
         txtCoins.setText("Coins: " + coins);
+
+        btnLogout = findViewById(R.id.btnLogOut);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RaceActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
